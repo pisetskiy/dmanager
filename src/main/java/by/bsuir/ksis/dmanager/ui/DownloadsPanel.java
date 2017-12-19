3198795e55140a109e64226aa0b04f35c8211791
Vladislav Pisetskiy <rockman52@ya.ru>
2017-12-19 22:05:42 +0300

    downloads tabs

---
+++ src/main/java/by/bsuir/ksis/dmanager/ui/DownloadsPanel.java
@@ -0,0 +1,11 @@
+package by.bsuir.ksis.dmanager.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+class DownloadsPanel extends JPanel {
+
+    DownloadsPanel() {
+        setPreferredSize(new Dimension(300, 300));
+    }
+}
--- src/main/java/by/bsuir/ksis/dmanager/ui/MainWindow.java
+++ src/main/java/by/bsuir/ksis/dmanager/ui/MainWindow.java
@@ -2,20 +2,36 @@ package by.bsuir.ksis.dmanager.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class MainWindow extends JFrame {
 
     private ControlPanel controlPanel = new ControlPanel();
+    private DownloadsPanel waitDownloadsPanel = new DownloadsPanel();
+    private DownloadsPanel runDownloadsPanel = new DownloadsPanel();
+    private DownloadsPanel finishDownloadsPanel = new DownloadsPanel();
 
     public MainWindow() throws HeadlessException {
         super("Менеджер загрузок");
-
         add(controlPanel, BorderLayout.NORTH);
-
+        createDownloadsTabs();
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
         requestFocus();
     }
+
+    private void createDownloadsTabs() {
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Ожидают", waitDownloadsPanel);
+        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+        tabbedPane.addTab("В процессе", runDownloadsPanel);
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+        tabbedPane.addTab("Завершены", finishDownloadsPanel);
+        tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+        add(tabbedPane, BorderLayout.CENTER);
+    }
+
+
 }
